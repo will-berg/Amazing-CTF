2971c22fc6c394b3efc605099b7c5747a108494a
Ferran Sanchez Llado <ferransll@gmail.com>
2023-09-24 18:37:11 +0200

    Docker setup
    - Dockerfile for all services (excluding db).
    - Docker compose file for easy initialization of services.
    - Added hacking website use nuxt3, where challanges should happen

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,14 @@ const favicon = require('serve-favicon')
 const path = require('path')
 
 const app = express();
-const port = 3000;
+const port = 5000;
 const rootPath = '/api';
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Has same favicon as frontend
-app.use(favicon(path.join(__dirname, '..', 'frontend', 'public', 'favicon.ico')))
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 app.get(rootPath, (_req, res) => {
     res.send('Welcome to the API for Amazing-CTF!');
