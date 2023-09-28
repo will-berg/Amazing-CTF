2e1bb6727b8912344451869f262c001d09f43ced
Ferran Sanchez Llado <ferransll@gmail.com>
2023-09-28 21:54:05 +0200

    Fix default layout
    Destroyed default layout by mistake

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="flex min-h-screen flex-col px-3 pt-3">
     <NavBar />
-    <main class="container mx-auto bg-white p-4">
-      <slot />
-    </main>
+    <div class="flex flex-grow flex-col md:flex-row">
+      <main class="container mx-auto bg-white p-4">
+        <slot />
+      </main>
+    </div>
     <Footer />
   </div>
 </template>
