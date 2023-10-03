385e37f10f125c499dac9191bfdfe3211870d055
Mehir <mehir00@yahoo.se>
2023-10-03 18:27:53 +0200

    working on xss stuff

--- hacking/components/infoCard/infoCard.vue
+++ hacking/components/infoCard/infoCard.vue
@@ -1,12 +1,23 @@
 <template>
-    <div class="bg-gray-800 rounded">
-    
+    <div class="bg-gray-800 border-double border-4 border-green-600 rounded-md flex flex-col gap-4 justify-center w-96 items-center text-center p-6">
+        <div class="text-white ">
+            {{ title }}
+        </div>
+        <div class="text-white">
+            {{ description }}
+        </div>
     </div>
 </template>
 
-<script>
+<script lang="ts" setup>
     defineProps({
-        title: String,
-        description: String,
-    });
+        title:{
+            type: String,
+            required: true
+        },
+        description:{
+            type: String,
+            required: true
+        }
+    })
 </script>
--- hacking/pages/XSS/index.vue
+++ hacking/pages/XSS/index.vue
@@ -1,30 +1,58 @@
 <template>
     <div>
-        <h2> Cross Site Scripting - XSS </h2>
-        <div>The goal of this hack is to inject and execute the following script into the webpage below:</div>
-        <div>&lt;script&gt;alert('Congratulations on completing the hack!');&lt;/script&gt;</div>
         <div class="">
-            <div class="navbar rounded-lg bg-gray-800 text-white flex items-center justify-between p-4 space-x-4">
+            <div class="navbar bg-gray-800 text-white flex items-center justify-between p-4 space-x-4">
                 <div class="text-xl">
-                    Welcome to HackUs.com - The best place to learn about hacking!
+                    XSS Hack Page
+                </div>
+                <div>
+                    <button @click="toggleShowCard">Show Info</button>
                 </div>
             </div>
-            <input placeholder="Search..." v-model="searchData"/>
-            <select class="" v-model="searchOption">
+            <div>
+                <form @submit.prevent="newPost">
+                    <input placeholder="Write a post..." v-model="postData" />
+                <!-- <select class="" v-model="searchOption">
                 <option disabled value="">Please select one</option>
                 <option v-for="item in items" :key="item">{{ item }}</option>
-            </select>
-            <button class="">Search</button>
-            <div>{{ searchData }}</div>
+            </select> -->
+                <button type="submit">Post</button>
+                </form>
+               
+            </div>
 
-            <InfoCard title="Cross Site Scripting - XSS" description=""/>
+            <div class="text-black" v-html="'<h1>Hello, world!</h1>'"></div>
+
+            <div v-if="showCard">
+                <InfoCard title="Cross Site Scripting - XSS" :description="descriptionText" />
+            </div>
+
+
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
-    const searchData = ref("");
-    const items = ["test1", "test2", "test3"];
-    const searchOption = ref("");
+const postData: Ref<string> = ref("");
+const posts: Ref<string[]> = ref([]);
+const showCard: Ref<boolean> = ref(true);
+const newPostData: Ref<string> = ref("");
+const toggleShowCard = () => {
+    showCard.value = !showCard.value;
+}
+
+const newPost = () => {
+    posts.value.push(postData.value);
+    console.log("posts array:" + posts.value)
+    console.log("postdata ref:" + postData.value)
+
+    newPostData.value = postData.value;
+    console.log("newPostData ref:" + newPostData.value)
+    postData.value = "";
+    
+    alert("You have succesfully posted a new post!")
+}
+
+const descriptionText: string = "Cross Site Scripting - XSS, the goal of this hack is to inject and execute the following script into the webpage: "
 
 </script>
