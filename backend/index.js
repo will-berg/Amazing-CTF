03fa69b22558fd34ef945c16165caf7db059c62d
Ferran Sanchez Llado <31955699+ferranSanchezLlado@users.noreply.github.com>
2023-10-10 12:27:42 +0200

    Fixed db docker +Challanges API
    - Database is now persisted across sessions.
    - Implemented basic Get methods for challenges API
    - Fixed backend connection so it connects to the proper port.

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,32 @@
 const express = require("express");
 const favicon = require("serve-favicon");
 const path = require("path");
-const app = express();
-const port = 5000;
-const rootPath = "/api";
-const db = require("./db/db");
-const TestModel = require("./models/testSchema");
-const registerRoute = require("./routes/register");
-const loginRoute = require("./routes/login");
 const cors = require("cors");
+
+const db = require("./db/db");
+
+// Create express app
+const app = express();
+const rootPath = "";
+const port = 5000;
+
+// Connect to database
 db();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// Has same favicon as frontend
+// Loads favicon
 app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
-//cors setup to allow for calls from frontend
+// Cors setup to allow for calls from frontend
 const corsOptions = {
   origin: "http://localhost:3000",
   credentials: true,
 };
 app.use(cors(corsOptions));
 
-
+// Root path
 app.get(rootPath, (_req, res) => {
   res.send("Welcome to the API for Amazing-CTF!");
 });
@@ -33,34 +35,8 @@ app.get(rootPath, (_req, res) => {
 const challenges = require("./routes/challenges");
 app.use(`${rootPath}/challenges`, challenges);
 
-app.get("/test", async (req, res) => {
-  try {
-    const test = await TestModel.find();
-    res.json(test);
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-app.post("/test", async (req, res) => {
-  console.log(req.body)
-  const newName = req.body.name;
-  if (!newName) {
-    return res.status(400).json({ error: 'Name is required' });
-  }
-  console.log(newName);
-  const newTest = new TestModel({
-    name: newName
-  });
-  console.log(newTest)
-  try {
-    const test = await newTest.save();
-    res.json(test);
-  } catch (err) {
-    console.log(err);
-  } 
-});
-
+const registerRoute = require("./routes/register");
+const loginRoute = require("./routes/login");
 app.use("/register", registerRoute)
 app.use("/login", loginRoute)
 
