531d59aefc110a297fe143cacf90f157199fbd66
malmisen <sebastien.malmberg@hotmail.com>
2023-10-11 19:58:38 +0200

    Changes to profile and minor changes to hackingpage

--- frontend/components/profile/ProfileCompletedHacks.vue
+++ frontend/components/profile/ProfileCompletedHacks.vue
@@ -1,17 +1,33 @@
 <template>
     <div>
-        <div class="mt-4">
-            <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-2"></div>
-            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[480px] mb-1.5"></div>
-            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-1.5"></div>
-            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[440px] mb-1.5"></div>
-            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[460px] mb-1.5"></div>
-            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
-            </div>
-            <span class="sr-only">Loading...</span>
+      <div v-if="hacks" class="card w-96 bg-base-100 shadow-xl">
+        <div class="card-body">
+          <h2 class="card-title">Completed Hacks</h2>
+          <ul>
+            <li v-for="hack in hacks" :key="hack">
+              <div class="completed-hack">
+                {{ hack }}
+                <span class="badge badge-success">✓</span>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-</template>
-
-<script lang="ts" setup>
-
-</script>
+  </template>
+  
+  <script lang="ts" setup>
+  import { defineProps } from 'vue';
+  
+  const props = defineProps<{
+    hacks: string[] | null;
+  }>();
+  </script>
+  
+<style scoped>
+.completed-hack {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
--- frontend/pages/profile/[username].vue
+++ frontend/pages/profile/[username].vue
@@ -10,8 +10,8 @@
         <h2>{{ user.username }}</h2>
         <h4>Total Score: {{ user.points }}</h4>
         <hr class="w-96 h-1 mx-auto my-4 bg-green-100 border-0 rounded md:my-10 dark:bg-gray-700">
-        <CompletedHacks :hacks="user.completedHacks" />
-        <UploadProfileImage 
+        <ProfileCompletedHacks :hacks="user.completedHacks" />
+        <ProfileUploadImage 
         class="modal-overlay"
         :openModal="open"
         @uploadImage="uploadProfileImage"
@@ -26,25 +26,27 @@
 import { useRoute } from "vue-router";
 import { User } from "@/types"
 import { ref } from 'vue';
+import { storeToRefs } from "pinia";
 
 const store = useUserStore()
 const image = ref<string>("");
 const open = ref<boolean>(false);
 
+//const {user} : User = storeToRefs(store);
 
-const user: User = ref<User>({
-  email: "example@example.com",
-  username: "Regularclip",
-  completedHacks: ['SQLInject', 'ReDos', 'XSS-easy', 'XSS-hard'],
-  points: 175, 
-});
+const user: User = store.user;
+console.log("User: ", user)
+// const user: User = ref<User>({
+//   email: "example@example.com",
+//   username: "Regularclip",
+//   completedHacks: ['SQLInject', 'ReDos', 'XSS-easy', 'XSS-hard'],
+//   points: 175, 
+// });
 
 
-const { pending } = useAsyncData('profileImage', async () => {
-  const queryParams = new URLSearchParams({ username: user.value.username });
-  const url = `http://localhost:5000/profile?${queryParams.toString()}`;
-  image.value = url;
-});
+const queryParams = new URLSearchParams({ username: user.username });
+const url = `http://localhost:5000/profile?${queryParams.toString()}`;
+image.value = url;
 
 const openUploadProfileImage = ():void => {
   console.log("click was registered")
@@ -59,16 +61,15 @@ const uploadProfileImage = async (image: File | null):Promise<void> => {
     return;
   }
   const formData = new FormData();
-  formData.append('username', user.value.username);
-  formData.append('image', image, `${user.value.username}_profile_image.png`);
+  formData.append('username', user.username);
+  formData.append('image', image, `${user.username}_profile_image.png`);
 
   const { data: responseData, error, pending } = await useFetch('http://localhost:5000/profile/image', {
         method: 'post',
         body: formData
     })
 
-  console.log(responseData.value)
-  console.log(error)
+  window.location.reload()
 }
 
 
