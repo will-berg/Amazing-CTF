b2dca11b8883dd7bd2006307f0dcc5c9464e96ef
Ferran Sanchez Llado <31955699+ferranSanchezLlado@users.noreply.github.com>
2023-10-16 18:52:42 +0200

    Fixed db connection
    First initialization failed to connect into the db in a single try. Now it tries 5 times before giving up.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,26 @@
 const mongoose = require("mongoose");
 
+const MAX_RETRIES = 5;
+
 const db = async () => {
-  try {
-    await mongoose.connect(
-      "mongodb://root:test@database:27017/amazing-ctf?authSource=admin",
-      { useNewUrlParser: true, useUnifiedTopology: true }
-    );
-    console.log("Connected to MongoDB");
-  } catch (err) {
-    console.log(err);
+  // First time could fail as MongoDB could be initializing
+  let retries = 0;
+  while (mongoose.connection.readyState !== 1) {
+    if (retries++ >= MAX_RETRIES) {
+      console.log("Could not connect to MongoDB");
+      process.exit(1);
+    }
+
+    console.log(`Connecting to MongoDB (try ${retries}/${MAX_RETRIES})`);
+    try {
+      await mongoose.connect(
+        "mongodb://root:test@database:27017/amazing-ctf?authSource=admin",
+        { useNewUrlParser: true, useUnifiedTopology: true }
+      );
+      console.log("Connected to MongoDB!!!");
+    } catch (err) {
+      console.log(err);
+    }
   }
 };
 
