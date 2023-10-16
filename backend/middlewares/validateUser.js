3ec060bf02f095dcd0084065f0fc7f02ceeba3b1
Mehir <mehir00@yahoo.se>
2023-10-16 12:41:42 +0200

    initial commit for moving hacks and getting points

--- backend/middlewares/validateUser.js
+++ backend/middlewares/validateUser.js
@@ -2,14 +2,14 @@ const jwt = require("jsonwebtoken");
 
 const validateUser = (req, res, next) => {
   //maybe check this better
-  const tokenHeader = req.header("Authorization");
+  const tokenHeader = req.headers["Authorization"];
+  console.log(tokenHeader)
   if (!tokenHeader) {
     console.log("no token")
     return res.status(401).json({ error: "Need to be logged in to access" });
   }
 
   const token = tokenHeader.split(" ")[1];
-
   jwt.verify(token, "Mehir123", (err, decoded) => {
     if (err) {
       console.log("token not valid")
