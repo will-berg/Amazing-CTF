b3c03969e33af7da539c6ecbd0d58ef33d981eb0
Ferran Sanchez Llado <31955699+ferranSanchezLlado@users.noreply.github.com>
2023-10-17 15:04:15 +0200

    Improved Pagination
    Fixed bug where user could write numbers outside the range

--- frontend/components/Pagination.vue
+++ frontend/components/Pagination.vue
@@ -3,7 +3,7 @@
     <button class="join-item btn btn-outline" :disabled="page === 1" @click="$emit('update:page', page - 1)">«</button>
     <div class="join-item flex justify-center items-center">
       <input class="input bg-base-200 w-16 text-center hover:border-red-500 hover:border-spacing-1" type="number" min="1"
-        :max="max_page" :value="page" @input="$emit('update:page', limit(($event.target as HTMLInputElement).value))">
+        :max="max_page" v-model="pageRef" @input="$emit('update:page', limit(($event.target as HTMLInputElement).value))">
       <div class="border-l-2 border-gray-500 h-full mx-0.5"></div>
       <div class="text-center w-16">{{ max_page }}</div>
     </div>
@@ -24,6 +24,13 @@ function limit(value: string): number {
   if (num > max_page) return max_page;
   return num;
 }
+
+// To avoid allowing the user to enter bigger numbers than the max_page or smaller than 1.
+// As component its only rerendered when the page changes, we need to use pageRef to make sure the input is updated.
+const pageRef = ref(page.toString());
+watch(pageRef, (newPage) => {
+  pageRef.value = limit(newPage).toString();
+});
 
 defineEmits<{
   (event: "update:page", page: number): void;
